23496c3148e8b750b792454282199705d3e9208a
zachar-AI <zachskihill@gmail.com>
2026-02-22 19:16:30 -0500

    added state to track if we are currently computing the bpm. will turn off with timeout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleTap() {
+    setIsActive(true);
+  }
+
   return (
     <>
       <h1>Your BPM is 128</h1>
-      <button>Tap Me</button>
+      <button
+      onPointerDown={handleTap}
+      >
+        Tap Me
+      </button>
+      <p>{isActive ? "active" : "not active"}</p>
     </>
   )
 }
