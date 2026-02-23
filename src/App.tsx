ccb8a8a13e345f9af4fbfd46a33136079080a8c2
zachar-AI <zachskihill@gmail.com>
2026-02-22 20:22:23 -0500

    bpm tracker is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,43 @@
 import { useState } from 'react'
+import { useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [activePresses, setActivePresses] =useState(0);
+  const [activePresses, setActivePresses] = useState(0);
   let isActive = activePresses === 0 ? false : true;
+  const last4TimesRef = useRef([0, 0, 0, 0]);
+
 
   function handleTap() {
     setActivePresses((activePresses) => activePresses + 1);
     setTimeout(handleTimeout, 5000);
+    last4TimesRef.current.shift();
+    last4TimesRef.current.push(Date.now());
   }
 
   function handleTimeout() {
     setActivePresses(activePresses => activePresses - 1);
   }
 
+  function calculateBpm(): number {
+    let sumDeltas = 0;
+    for (let i = 1; i < last4TimesRef.current.length; i++) {
+      sumDeltas += last4TimesRef.current[i] - last4TimesRef.current[i-1];
+    }
+    const msPerBeat = sumDeltas / (last4TimesRef.current.length - 1);
+    const bpms = 1 / msPerBeat;
+    const bpm = Math.round(bpms*1000*60);
+    return bpm;
+  }
+
   return (
     <>
-      <h1>Your BPM is 128</h1>
+      <h1>Your BPM is {calculateBpm()}</h1>
       <button
       onPointerDown={handleTap}
       >
         Tap Me
       </button>
-      <p>{isActive ? "active" : "not active"}</p>
     </>
   )
 }
