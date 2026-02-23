4301a13c2233ca106816f004f09d66e133372878
zachar-AI <zachskihill@gmail.com>
2026-02-22 20:41:27 -0500

    bpm now goes to 0 when inactive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import { useRef } from 'react'
-import './App.css'
+import { useState } from "react";
+import { useRef } from "react";
+import "./App.css";
 
 function App() {
   const [activePresses, setActivePresses] = useState(0);
-  let isActive = activePresses === 0 ? false : true;
   const last4TimesRef = useRef([0, 0, 0, 0]);
-
 
   function handleTap() {
     setActivePresses((activePresses) => activePresses + 1);
@@ -16,30 +14,29 @@ function App() {
   }
 
   function handleTimeout() {
-    setActivePresses(activePresses => activePresses - 1);
+    setActivePresses((activePresses) => activePresses - 1);
   }
 
   function calculateBpm(): number {
+    if (activePresses < 4) {
+      return 0;
+    }
     let sumDeltas = 0;
     for (let i = 1; i < last4TimesRef.current.length; i++) {
-      sumDeltas += last4TimesRef.current[i] - last4TimesRef.current[i-1];
+      sumDeltas += last4TimesRef.current[i] - last4TimesRef.current[i - 1];
     }
     const msPerBeat = sumDeltas / (last4TimesRef.current.length - 1);
     const bpms = 1 / msPerBeat;
-    const bpm = Math.round(bpms*1000*60);
+    const bpm = Math.round(bpms * 1000 * 60);
     return bpm;
   }
 
   return (
     <>
       <h1>Your BPM is {calculateBpm()}</h1>
-      <button
-      onPointerDown={handleTap}
-      >
-        Tap Me
-      </button>
+      <button onPointerDown={handleTap}>Tap Me</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
